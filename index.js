a5113df45b5560ad79ec9b9437a71ae141a1ad4f
Spencer Brown <me@spencer.sx>
2016-12-08 23:02:36 -0500

    All the things

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const request = require('request');
+
+app.post('/new-app', (req, res, next) => {
+  if (!req.body.githubURL || !req.body.subdomain || !req.body.userId) {
+    res.status(400).end();
+  }
+
+  const url = 'bart.usb.cs.purdue.edu:3000';
+
+  request.post(url, {
+    form: {
+      githubURL: req.body.githubURL,
+      subdomain: req.body.subdomain,
+      userId: req.body.userId
+    }
+  }, (error, response, body) => {
+    if (!error) {
+      res.status(200).end();
+    } else {
+      res.status(500).end();
+    }
+  });
+});
+
+app.listen(1337, () => {
+  console.log("I'm alive!");
+});
